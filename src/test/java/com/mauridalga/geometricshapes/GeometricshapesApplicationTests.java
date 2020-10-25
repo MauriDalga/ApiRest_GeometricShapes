49763ceb930d898f751f220ff916a3ea428527e1
Mauricio Dalgalarrondo <mauridalga@hotmail.com>
2020-10-24 22:42:58 -0300

    Spring Boot Web Api Project Created

---
+++ src/test/java/com/mauridalga/geometricshapes/GeometricshapesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauridalga.geometricshapes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeometricshapesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
